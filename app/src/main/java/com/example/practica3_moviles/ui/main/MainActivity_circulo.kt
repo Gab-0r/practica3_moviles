c49f7637ba752518b982478a28b016478a43affe
Sebastian Bernal <sebastian.bernalc@udea.edu.co>
2022-06-27 22:06:57 -0500

    Se agrego la interfaz de circulo y se configuro el boton para pasar a la actividad del circulo

--- app/src/main/java/com/example/practica3_moviles/ui/main/MainActivity_circulo.kt
+++ app/src/main/java/com/example/practica3_moviles/ui/main/MainActivity_circulo.kt
@@ -4,20 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.example.practica3_moviles.R
 import com.example.practica3_moviles.databinding.ActivityMainBinding
+import com.example.practica3_moviles.databinding.ActivityMainCirculoBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var mainBinding: ActivityMainBinding
+class MainActivity_circulo : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainCirculoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        mainBinding = ActivityMainCirculoBinding.inflate(layoutInflater)
         val view = mainBinding.root
         setContentView(view)
 
     }
-    public fun actividadcirculo(view: View){
-        val circuloact=Intent(this,MainActivity_circulo::class.java)
-        startActivity(circuloact)
-    }
-
 }
