8e1ab0a8ae9a4befdf47c1bb9b32ede8922a23b7
Alexander E. Montoya <alexanderem12@gmail.com>
2022-06-28 21:00:31 -0500

    Feature Area y Perimetro del Triangulo, Alexander Espinosa Montoya

--- app/src/main/java/com/example/practica3_moviles/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/practica3_moviles/ui/main/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.example.practica3_moviles.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.practica3_moviles.R
+import com.example.practica3_moviles.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)//colocado la actividad principal
+        val view = mainBinding.root //colocado la actividad principal
+        setContentView(view)
+
+        mainBinding.buttonTriangulo.setOnClickListener {
+            irActividadTriangulo()
+        }
+    }
+
+    private fun irActividadTriangulo() {// funcion para lanzar la actividad triangulo
+        val intent =
+            Intent(this, ActivityTriangulo::class.java) //a intent le asigno la actividad
+        startActivity(intent) // me voy a ala actividad
     }
 }
+
