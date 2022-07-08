8936d232a013a60292c97c397ff73fb4f73aa972
Gabriel <j_gabrielorozco@live.com>
2022-07-07 20:32:23 -0500

    Interfaz gráfica del cuadrado preparada.

--- app/src/main/java/com/example/practica3_moviles/ui/main/ActivityCuadrado.kt
+++ app/src/main/java/com/example/practica3_moviles/ui/main/ActivityCuadrado.kt
@@ -2,13 +2,20 @@ package com.example.practica3_moviles.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.example.practica3_moviles.databinding.ActivityCuadradoBinding
 
-private lateinit var cuadradoBinding: ActivityCuadradoBinding
-
 class ActivityCuadrado : AppCompatActivity() {
+    private lateinit var cuadradoBinding: ActivityCuadradoBinding
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cuadrado)
+        cuadradoBinding = ActivityCuadradoBinding.inflate(layoutInflater)
+        val view = cuadradoBinding.root
+        setContentView(view)
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
     }
 }
