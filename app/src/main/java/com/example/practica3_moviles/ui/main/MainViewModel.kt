9cf2a05589961938b54d34a234dda58b95b15bcf
Juan <juang15x@gmail.com>
2022-07-13 06:32:16 -0500

    Finished

--- app/src/main/java/com/example/practica3_moviles/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/practica3_moviles/ui/main/MainViewModel.kt
@@ -1,6 +1,20 @@
 package com.example.practica3_moviles.ui.main
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel() {
+    fun getResult(width: String, height: String) {
+        val widthNumeric = width.toFloat()
+        val heightNumeric = height.toFloat()
+        perimetroMutableViewModel_rectangulo.value = 2 * widthNumeric + 2 * heightNumeric
+        areaMutableViewModel_rectangulo.value = widthNumeric * heightNumeric
+    }
+
+
+    private val perimetroMutableViewModel_rectangulo: MutableLiveData<Float> = MutableLiveData()
+    val perimetroLiveData_rectangulo: LiveData<Float> = perimetroMutableViewModel_rectangulo
+    private val areaMutableViewModel_rectangulo: MutableLiveData<Float> = MutableLiveData()
+    val areaLiveData_rectangulo: LiveData<Float> = areaMutableViewModel_rectangulo
 }
--- app/src/main/java/com/example/practica3_moviles/ui/main/RectanguloActivity.kt
+++ app/src/main/java/com/example/practica3_moviles/ui/main/RectanguloActivity.kt
@@ -2,10 +2,12 @@ package com.example.practica3_moviles.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.example.practica3_moviles.databinding.ActivityRectanguloBinding
 
 class RectanguloActivity : AppCompatActivity() {
     private lateinit var rectanguloBinding: ActivityRectanguloBinding
+    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,8 +15,25 @@ class RectanguloActivity : AppCompatActivity() {
         val view = rectanguloBinding.root
         setContentView(view)
 
+        mainViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+
         with(rectanguloBinding){
-            //resultTextView.text = ""
+            mainViewModel.perimetroLiveData_rectangulo.observe(this@RectanguloActivity){
+                    perimetro_rectangulo ->
+                perimetroTextView.text = perimetro_rectangulo.toString()
+            }
+
+            mainViewModel.areaLiveData_rectangulo.observe(this@RectanguloActivity){
+                    area_rectangulo ->
+                areaTextView.text = area_rectangulo.toString()
+            }
+
+            operateButton.setOnClickListener {
+                val width = widthEditText.text.toString()
+                val height = heightEditText.text.toString()
+
+                mainViewModel.getResult(width, height)
+            }
         }
     }
 }
