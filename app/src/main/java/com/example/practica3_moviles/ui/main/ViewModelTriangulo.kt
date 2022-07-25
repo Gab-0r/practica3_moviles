eac7ed49a9a89ad491ee8b6da235247809a7b57e
Alexander Espinosa M <alexander.espinosa@udea.edu.co>
2022-07-25 16:41:54 -0500

    Correciones en la actividad Triangulo, Alexander Espinosa Montoya

--- app/src/main/java/com/example/practica3_moviles/ui/main/ViewModelTriangulo.kt
+++ app/src/main/java/com/example/practica3_moviles/ui/main/ViewModelTriangulo.kt
@@ -11,22 +11,34 @@ class ViewModelTriangulo: ViewModel() {
     private val CalculoTriangulo: MutableLiveData<String> = MutableLiveData()
     val CalculoTrianguloDone: LiveData<String> = CalculoTriangulo
 
+    private val Error : MutableLiveData<String> = MutableLiveData()
+    val error : LiveData<String> = Error
+
     //funciones para trabajar
 
     fun Calucular_Triangulo(ladoA: String, ladoB: String, ladoC: String){
 
-        val a = ladoA.toDouble()
-        val b = ladoB.toDouble()
-        val c = ladoC.toDouble()
+        if (ladoA.isEmpty() || ladoB.isEmpty() || ladoC.isEmpty()){
+            Error.value = "Faltan datos"
+            return
+        }
+        else{
+            val a = ladoA.toDouble()
+            val b = ladoB.toDouble()
+            val c = ladoC.toDouble()
 
-        val area: Double
-        val perimetro: Double
-        val s = (a+b+c)/2
-
-        area = sqrt(s*((s-a)*(s-b)*(s-c)))
-        perimetro = 2*s
-
-        CalculoTriangulo.value = " Area: $area \n Perimetro: $perimetro"
+            val area: Double
+            val perimetro: Double
+            val s = (a+b+c)/2
+            val raiz = s*((s-a)*(s-b)*(s-c))
+            if (raiz >= 0){
+                area = sqrt(raiz)
+                perimetro = 2*s
+                CalculoTriangulo.value = " Area: $area \n Perimetro: $perimetro"
+            }else{
+                Error.value = "Datos del triangulo no validos"
+            }
+        }
     }
 
 }
